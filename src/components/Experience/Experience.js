4a4403717f40b86f40ddf96c231896c9d8a7a09b
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-03-26 19:46:50 -0400

    deploy

--- src/components/Experience/Experience.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class Experience extends Component {
-  render() {
-    return <div>General Assembly WDI</div>
-  }
-}
-
-export default Experience
--- src/components/Footer/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return <footer>This is my footer.</footer>
-}
-
-export default Footer
