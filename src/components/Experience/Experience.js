81bb453b4bb75d54d942a00fe85e600440c377b0
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-03-26 18:16:07 -0400

    react fix

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class Experience extends Component {
   render() {
-    return <div>This is my experience section.</div>
+    return <div>General Assembly WDI</div>
   }
 }
 
