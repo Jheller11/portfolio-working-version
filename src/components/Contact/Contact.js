ac1b8a954bb23a1a52ae73df2a521f07897b26ed
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-05-02 12:43:53 -0400

    style update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,32 +1,73 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const contact = {
-  email: 'heller.jeffrey@gmail.com',
-  github: 'https://github.com/Jheller11',
-  linkedIn: 'https://www.linkedin.com/in/jeffreyaheller/'
-}
-const Contact = () => {
-  return (
-    <div>
-      <p className="center">
-        Thanks for visiting my page. Please use any of the links below to
-        contact me:
-      </p>
-      <div className="contact">
-        <a href="mailto:heller.jeffrey@gmail.com">
-          <img className="icon large" alt="email" src="/email.png" />
-          heller.jeffrey@gmail.com
-        </a>
-        <a href={contact.github}>
-          <img className="icon" alt="github" src="/github.png" /> GitHub
-        </a>
-        <a href={contact.linkedIn}>
-          <img className="icon" alt="linkedin" src="/linkedin.png" /> LinkedIn
-        </a>
+class Contact extends Component {
+  constructor() {
+    super()
+    this.state = {
+      email: [
+        'mailto:heller.jeffrey@gmail.com',
+        'Email: heller.jeffrey@gmail.com'
+      ],
+      github: ['https://github.com/Jheller11', 'GitHub Profile: Jheller11'],
+      linkedIn: [
+        'https://www.linkedin.com/in/jeffreyaheller/',
+        'LinkedIn Profile: jeffreyaheller'
+      ],
+      activeContact: ''
+    }
+    this.setActive = this.setActive.bind(this)
+    this.clearActive = this.clearActive.bind(this)
+  }
+
+  setActive(e) {
+    let input = e.target.alt
+    if (this.state.activeContact !== input) {
+      this.setState({
+        activeContact: input
+      })
+    }
+  }
+
+  clearActive() {
+    this.setState({
+      activeContact: ''
+    })
+  }
+  render() {
+    return (
+      <div onMouseLeave={this.clearActive}>
+        <p className="center" />
+        <div className="contact">
+          <a className="contact-row" href="mailto:heller.jeffrey@gmail.com">
+            <img
+              onMouseOver={this.setActive}
+              className="icon large"
+              alt={this.state.email[1]}
+              src="/email.png"
+            />
+          </a>
+          <a className="contact-row" href={this.state.github}>
+            <img
+              onMouseOver={this.setActive}
+              className="icon"
+              alt={this.state.github[1]}
+              src="/github.png"
+            />
+          </a>
+          <a className="contact-row" href={this.state.linkedIn}>
+            <img
+              onMouseOver={this.setActive}
+              className="icon"
+              alt={this.state.linkedIn[1]}
+              src="/linkedin.png"
+            />{' '}
+          </a>
+        </div>
+        <div className="center active">{this.state.activeContact}</div>
+        <div id="space" />
       </div>
-      <div id="space" />
-    </div>
-  )
+    )
+  }
 }
 
 export default Contact
